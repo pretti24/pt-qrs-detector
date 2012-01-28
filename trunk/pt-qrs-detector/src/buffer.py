b8ce5e4f88575af02a6f19b11a1c18b98c592a01
Sergio2pi@gmail.com <Sergio2pi@gmail.com@e092d560-1a90-a740-5bce-1cb6071af671>
2012-01-28 18:13:52 +0000

    buffer.py

--- trunk/pt-qrs-detector/src/buffer.py
+++ trunk/pt-qrs-detector/src/buffer.py
@@ -1,22 +1,38 @@
 '''
 Created on Jan 27, 2012
-
-@author: sergio
+@author: Sergio
 '''
-import collections
+from collections import deque
+import numpy
 
-class buffer(collections):
+class buffer(deque):
     '''
-    classdocs
+    Buffer de longitud fija. Inicialmente lleno de 0.0
     '''
-
-    def __init__(self,len):
+    def __init__(self,length):
         '''
         Constructor
         '''
+        self.len = length
+        self.buffer = deque(numpy.zeros(length))
         
-
     def append(self,elemento):
-        
-
+        self.buffer.append(elemento)
+        return self.buffer.popleft()
+    
+    def pop(self,elemento=0.0):
+        self.buffer.append(elemento)
+        return self.buffer.popleft()
+    
+    def len(self):
+        return self.length
+    
+    def purge(self):
+        array = numpy.array(self.buffer)
+        self.buffer = deque(numpy.zeros(self.length))
+        return array
+    
+    def get(self,position):
+        array = numpy.array(self.buffer)
+        return array[position]
         
--- trunk/pt-qrs-detector/src/test.py
+++ trunk/pt-qrs-detector/src/test.py
@@ -4,13 +4,10 @@ Created on Jan 27, 2012
 @author: sergio
 '''
 from wfdbtools import rdsamp, rdann, plot_data
-from pprint import pprint
-import pylab
-from scipy import signal
 import numpy
-from collections import deque
+from buffer import buffer
 
-###Senal
+### Senal
 record  = '104'
 data, info = rdsamp(record, 301, 320)
 ann = rdann(record, 'atr', 301, 320)
@@ -25,5 +22,19 @@ ann2 = ann[:, 1]
 Fs = info['samp_freq']
 #1/8T(-z^2/32-5z/32-5/8z-7/8z^2-9/8z^3-21/16z^4-21/16z^5-9/8z^6-7/8z^7-5/8z^8-3/8z^9-5/32z^10-1/32z^11+1/z^14+4/z^15+7/z^16+8/z^17+8/z^18+8/z^19+6/z^20-6/z^22-8/z^23-8/z^24-8/z^25-7/z^26-4/z^27-1/z^28+1/32z^30+5/32z^31+3/8z^32+5/8z^33+7/8z^34+9/z^35+21/16z^36+21/16z^37+9/8z^38+7/8z^39+5/8z^40+3/8z^41+5/32z^42+1/32z^43-3/8)
 
+### Carga inicial de la senal a un emulador de lectura en tiempo real
+length = len(signal1)
+signal = buffer(length)
+for sample in signal1:
+    signal.append(sample)
 
+### Carga inicial del buffer  
+buffer = buffer(42)    
+for i in range(42):
+    sample = signal.pop()
+    buffer.append(sample)
 
+### Procesamiento
+output = [0,0,0]
+while 1:
+    output = 
